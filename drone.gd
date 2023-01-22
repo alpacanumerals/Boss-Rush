d7b22ca6d939fa4b470a8a9dba52df532895eb03
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2023-01-22 19:22:15 +0000

    drone moves (maybe too much drag)

---
+++ drone.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var direction_to_mouse
+var velocity = Vector2()
+var accel = -10
+var resistance = 0.0001
+
+func _ready():
+    $AnimatedSprite.play()
+
+func _physics_process(delta):
+    direction_to_mouse = global_position.angle_to_point(get_global_mouse_position())
+    
+    velocity = velocity + get_thrust_vector()
+    velocity = velocity + get_drag_vector(velocity)
+    
+    velocity = move_and_slide(velocity)
+
+func get_thrust_vector():
+    return Vector2(accel, 0).rotated(direction_to_mouse)
+    
+func get_drag_vector(velocity):
+    var speed = velocity.length()
+    var drag = speed * speed * resistance
+    var drag_vector = -1 * velocity.normalized() * drag
+    return drag_vector
