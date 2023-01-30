2ec6c9979f8001d6245845f893a1cb9da80c658f
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2023-01-30 18:39:59 -0500

    Update truck.gd

--- truck.gd
+++ truck.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var accel_f= 2
 var accel_r= 8
+var decel = 4
 var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -12,13 +13,15 @@ func _ready():
 
 func _physics_process(delta):
     process_movement_input()
-    velocity = move_and_slide(velocity)
+    velocity = move_and_slide(velocity)*0.98
 
 func process_movement_input():
     if Input.is_action_pressed("ui_right"):
         velocity.x += accel_f
     if Input.is_action_pressed("ui_left"):
         velocity.x -= accel_r
-    velocity.x = clamp(velocity.x, -160, 80)
+    #else: 
+    #    velocity.x = velocity.x * 0.97
+    velocity.x = clamp(velocity.x, -160, 90)
     #if self.position.x <= 63 or self.position.x >=585:
     #    velocity.x = 0
