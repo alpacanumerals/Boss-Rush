21bfa4b186475e5ada4fd65a65702ed5cd3305c1
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2023-01-30 17:14:37 -0500

    truck

--- truck.gd
+++ truck.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var accel = 1
+var accel = 5
 var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -18,4 +18,4 @@ func process_movement_input():
         velocity.x += accel
     if Input.is_action_pressed("ui_left"):
         velocity.x -= accel
-    velocity.x = clamp(velocity.x, -120, 120)
+    velocity.x = clamp(velocity.x, -150, 90)
