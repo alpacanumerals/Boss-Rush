689f425ab3a4c611380df6268ed004463eda01d8
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2023-02-11 00:22:18 +0000

    car progression

--- car.gd
+++ car.gd
@@ -1,18 +1,21 @@
 extends KinematicBody2D
 
+signal killed
 var explosion = preload("res://Explosion.tscn")
 
 var velocity = Vector2(-0.1, 0)
-var live = true
+var live = false
+var dead = false
 
 var explosion_timer = 0
 
 func _ready():
     $AnimatedSprite.play("default")
+    connect("killed", get_parent(), "_on_Car_killed")
 
 func _physics_process(delta):
     move_and_collide(velocity)
-    if !live:
+    if dead:
         explosion_timer += 1
         if explosion_timer > 10:
             explosion_timer = 0
@@ -25,12 +28,14 @@ func _on_Turret_killed():
         if turret.live:
             turrets_alive += 1
     if turrets_alive <= 0:
+        emit_signal("killed")
         car_explode()
 
 func car_explode():
     velocity = Vector2(-2, 0)
     $AnimatedSprite.play("broken")
     live = false
+    dead = true
 
 func scatter_explosion():
     var x_loc = (randi() % 400) - 200
@@ -38,3 +43,7 @@ func scatter_explosion():
     var e = explosion.instance()
     add_child(e)
     e.set_position(Vector2(x_loc, y_loc))
+
+func activate():
+    live = true
+    $Turret1.activate()
--- train.gd
+++ train.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var car = preload("res://Car.tscn")
-
+var current_car = 0
 
 func _ready():
     add_car(0)
     add_car(1)
     add_car(2)
+    activate(current_car)
     
 func _physics_process(delta):
     pass
@@ -15,4 +16,14 @@ func add_car(number):
     var c = car.instance()
     add_child(c)
     c.set_position(Vector2(number*410, 0))
+    c.add_to_group("cars")
+    
+func _on_Car_killed():
+    current_car += 1
+    activate(current_car)
+
+func activate(carriage):
+    var cars = get_tree().get_nodes_in_group("cars")
+    if carriage < cars.size():
+        cars[carriage].activate()
     
