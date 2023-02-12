c5f39654482dfd8040b7a508774462e644ef149c
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2023-02-12 17:50:38 -0500

    cursor

---
+++ player/Cursor.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+const speed = 250
+
+func _ready():
+    pass # Replace with function body.
+
+func _process(delta):
+    process_movement_input()
+    velocity = move_and_slide(velocity)
+    
+func process_movement_input():
+    velocity = Vector2()
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
