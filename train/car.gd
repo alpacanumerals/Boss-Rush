a2c8e710ad75e02e4d0a64997c8b78fa5971accf
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2023-02-12 17:18:45 +0000

    burning train cars

--- train/car.gd
+++ train/car.gd
@@ -2,11 +2,17 @@ extends KinematicBody2D
 
 signal killed
 var explosion = preload("res://projectiles/Explosion.tscn")
+var fire = preload("res://projectiles/Fire.tscn")
 
 var live = false
 var dead = false
+var burnout = false
+
+var explosion_interval = 0.1
+var burnout_limit = 5
 
 var explosion_timer = 0
+var burnout_timer = 0
 
 func _ready():
     $AnimatedSprite.play("default")
@@ -14,10 +20,14 @@ func _ready():
 
 func _physics_process(delta):
     if dead:
-        explosion_timer += 1
-        if explosion_timer > 10:
+        explosion_timer += delta
+        burnout_timer += delta
+        if explosion_timer > explosion_interval && burnout_timer < burnout_limit:
             explosion_timer = 0
             scatter_explosion()
+        if burnout_timer > burnout_limit && !burnout:
+            burnout = true
+            scatter_fires()
 
 func _on_Turret_killed():
     var turrets = get_tree().get_nodes_in_group("turrets")
@@ -40,6 +50,17 @@ func scatter_explosion():
     var e = explosion.instance()
     add_child(e)
     e.set_position(Vector2(x_loc, y_loc))
+
+func scatter_fires():
+    for i in range(10):
+        scatter_fire()
+    
+func scatter_fire():
+    var x_loc = (randi() % 400) - 200
+    var y_loc = (randi() % 100) - 50
+    var f = fire.instance()
+    add_child(f)
+    f.set_position(Vector2(x_loc, y_loc))
 
 func activate():
     live = true
