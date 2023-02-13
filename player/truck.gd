8a1b2b00948cee7b2936aeb1e1450ec47b9a2aca
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2023-02-13 04:59:06 -0500

    truck movement

--- player/truck.gd
+++ player/truck.gd
@@ -16,9 +16,9 @@ func _physics_process(delta):
     velocity = move_and_slide(velocity)*0.98
 
 func process_movement_input():
-    if Input.is_action_pressed("ui_right"):
+    if Input.is_action_pressed("truck_left"):
         velocity.x += accel_f
-    if Input.is_action_pressed("ui_left"):
+    if Input.is_action_pressed("truck_right"):
         velocity.x -= accel_r
     #else: 
     #    velocity.x = velocity.x * 0.97
